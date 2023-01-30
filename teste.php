2bff74114e24a90ea9a09bb498ee48d0f73100e0
thainara pires <104094316+thainarapires@users.noreply.github.com>
2023-01-30 16:37:46 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -13,14 +13,14 @@
             <h1 style="color:rgb(255, 81, 0)"> Cursos:</h1>
             <h2 style="color:black"> 
                 <a href="https://www.sp.senac.br/cursos-tecnicos/curso-tecnico-em-informatica"> Técnico em informática  </a> <br>
-                <img src="pc.png"><br>
+                <img src="https://cdn-icons-png.flaticon.com/512/316/316325.png"><br>
                 <a href="https://www.sp.senac.br/cursos-tecnicos/curso-tecnico-em-enfermagem"> Enfermagem  </a> <br>
-                <img src="enfe.png" width="230"<br>
+                <img src="https://cdn-icons-png.flaticon.com/512/1616/1616539.png" width="230"<br>
 			
 			<h3 style="color:black"> Linkedin: <a href="https://www.linkedin.com/in/robson-vaamonde-0b029028/">Thainara Pires</a><br>
 			SITE: <a href="https://github.com/thainarapires">github.com/thainarapires</a><br>
 			INSTAGRAM: <a href="https://www.instagram.com/thaiinara.pires/">Thaiinara.Pires</a><br> </h3>
-            
+
 		</body>
 	</html>
 

b8c01c755973043c5511b0370501f826c622a4a0
thainara pires <104094316+thainarapires@users.noreply.github.com>
2023-01-30 16:16:16 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,19 +1,26 @@
-<html>
-	<html lang="pt-br">
+<html lang="pt-br">
 		<head>
-			<title> Teste da Linguagem HTML</title>
+			<title> Site teste da Thainara, com estilo SENAC</title>
 			<meta charset="utf-8">
-            <img src="senac.png.png" height="60%" width="30%"
             
 		</head>
 		<body 
         style="background-color:rgb(250, 240, 228)">
-		<h1 style="color:black" align="middle">SENAC TATUAPÉ</h1>
+		<img src="senac.png.png" height="60%" width="30%">
+        <u> <h1 style="color:black" align="middle">SENAC TATUAPÉ</h1></u>
         
+
+            <h1 style="color:rgb(255, 81, 0)"> Cursos:</h1>
+            <h2 style="color:black"> 
+                <a href="https://www.sp.senac.br/cursos-tecnicos/curso-tecnico-em-informatica"> Técnico em informática  </a> <br>
+                <img src="pc.png"><br>
+                <a href="https://www.sp.senac.br/cursos-tecnicos/curso-tecnico-em-enfermagem"> Enfermagem  </a> <br>
+                <img src="enfe.png" width="230"<br>
 			
 			<h3 style="color:black"> Linkedin: <a href="https://www.linkedin.com/in/robson-vaamonde-0b029028/">Thainara Pires</a><br>
 			SITE: <a href="https://github.com/thainarapires">github.com/thainarapires</a><br>
-			INSTAGRAM: <a href="https://www.instagram.com/thaiinara.pires/">Thaiinara Pires</a><br>
+			INSTAGRAM: <a href="https://www.instagram.com/thaiinara.pires/">Thaiinara.Pires</a><br> </h3>
+            
 		</body>
 	</html>
 
